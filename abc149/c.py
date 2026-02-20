e9a70415a263a22e103a9d58eb794711faa39289
yuukakageyama <160180214+yuukakageyama@users.noreply.github.com>
2026-02-21 02:28:04 +0900

    Correct range for prime number checking
    Fix prime number check by adjusting range in loop.

--- abc149/c.py
+++ abc149/c.py
@@ -2,10 +2,13 @@ import math
 X = int(input())
 
 for i in range(X, 100004):
+  if i < 2:
+    continue  
   is_prime = True
-  for j in range(2, math.isqrt(i)):
+  for j in range(2, math.isqrt(i) + 1):
     if i % j == 0:
       is_prime = False
+      break
   if is_prime:
     print(i)
     exit()
